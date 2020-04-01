889fd1297058e7e91d48cdcb68a5f3a0601f0bb3
Leo Yan <leo.yan@linaro.org>
2020-04-01 22:46:36 +0800

    ilm: test: Add lock test case for pytest
    Signed-off-by: Leo Yan <leo.yan@linaro.org>

--- test/ilm_test.py
+++ test/ilm_test.py
@@ -4,6 +4,7 @@ import errno
 import io
 import os
 import time
+import uuid
 
 import pytest
 
@@ -13,6 +14,34 @@ def test_lockspace(ilm_daemon):
     ret, s = ilm.ilm_connect()
     assert ret == 0
     assert s > 0
+
+    ret = ilm.ilm_disconnect(s)
+    assert ret == 0
+
+def test_lock(ilm_daemon):
+    ret, s = ilm.ilm_connect()
+    assert ret == 0
+    assert s > 0
+
+    lock_id = ilm.idm_lock_id()
+    lock_id.vg_uuid = "1111222233334444"
+    lock_id.lv_uuid = "0123456789abcdef"
+
+    lock_op = ilm.idm_lock_op()
+    lock_op.mode = 0
+    lock_op.drive_name = 2
+    drive = lock_op.get_array_element(0)
+    drive = "/dev/sda1"
+    drive = lock_op.get_array_element(1)
+    drive = "/dev/sda2"
+    lock_op.timeout = 0
+    lock_op.quiescent = 0
+
+    ret = ilm.ilm_lock(s, lock_id, lock_op)
+    assert ret == 0
+
+    ret = ilm.ilm_unlock(s, lock_id)
+    assert ret == 0
 
     ret = ilm.ilm_disconnect(s)
     assert ret == 0
