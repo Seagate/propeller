3d9e28a944a7759971fdf1ca546d12bb93af7851
Scott Howe <scott.d.howe@seagate.com>
2022-12-05 14:08:27 -0600

    FOSS-461: Reorganized header to reflect work needed on this branch (mostly in new, commented out lines).  Looks like only SOME of the SCSI idm read apis were implemented asynchronously.

--- src/idm_nvme_api.h
+++ src/idm_nvme_api.h
@@ -19,6 +19,9 @@
 
 //new
 int nvme_async_idm_get_result(uint64_t handle, int *result);
+// int nvme_async_idm_get_result_host_state(uint64_t handle, int *host_state, int *result);
+// int nvme_async_idm_get_result_lock_count(uint64_t handle, int *count, int *result);
+// int nvme_async_idm_get_result_lock_mode(uint64_t handle, int *mode, int *result);
 
 int nvme_async_idm_lock(char *lock_id, int mode, char *host_id,
                         char *drive, uint64_t timeout, uint64_t *handle);
@@ -32,6 +35,11 @@ int nvme_async_idm_lock_refresh(char *lock_id, int mode, char *host_id,
                                 char *drive, uint64_t timeout, uint64_t *handle);
 int nvme_async_idm_lock_renew(char *lock_id, int mode, char *host_id,
                                 char *drive, uint64_t timeout, uint64_t *handle);
+// int nvme_async_idm_read_lock_count(char *lock_id, char *host_id, int *count, int *self,
+//                                   char *drive, uint64_t *handle);
+// int nvme_async_idm_read_lock_mode(char *lock_id, int *mode, char *drive, uint64_t *handle);
+// int nvme_async_idm_read_lvb(char *lock_id, char *host_id, char *lvb, int lvb_size, char *drive,
+//                             uint64_t *handle);
 int nvme_async_idm_unlock(char *lock_id, int mode, char *host_id,
                           char *lvb, int lvb_size, char *drive, uint64_t *handle);
 
@@ -47,6 +55,13 @@ int nvme_sync_idm_lock_refresh(char *lock_id, int mode, char *host_id,
                                char *drive, uint64_t timeout);
 int nvme_sync_idm_lock_renew(char *lock_id, int mode, char *host_id,
                              char *drive, uint64_t timeout);
+// int nvme_sync_idm_read_host_state(char *lock_id, char *host_id, int *host_state, char *drive);
+// int nvme_sync_idm_read_lock_count(char *lock_id, char *host_id, int *count, int *self,
+//                                   char *drive);
+// int nvme_sync_idm_read_lock_mode(char *lock_id, int *mode, char *drive);
+// int nvme_sync_idm_read_lvb(char *lock_id, char *host_id, char *lvb, int lvb_size, char *drive);
+// int nvme_sync_idm_read_mutex_group(char *drive, idmInfo **info_ptr, int *info_num);
+// int nvme_sync_idm_read_mutex_num(char *drive, unsigned int *num);
 int nvme_sync_idm_unlock(char *lock_id, int mode, char *host_id,
                          char *lvb, int lvb_size, char *drive);
 
@@ -58,8 +73,26 @@ int _init_lock_destroy(char *lock_id, int mode, char *host_id, char *drive,
                        nvmeIdmRequest **request_idm);
 int _init_lock_refresh(char *lock_id, int mode, char *host_id, char *drive,
                        uint64_t timeout, nvmeIdmRequest **request_idm);
+// int _init_read_host_state(char *lock_id, char *host_id, int *host_state,
+//                           char *drive, nvmeIdmRequest **request_idm);
+// int _init_read_lock_count(char *lock_id, char *host_id, int *count, int *self,
+//                           char *drive, nvmeIdmRequest **request_idm);
+// int _init_read_lock_mode(char *lock_id, int *mode,
+//                          char *drive, nvmeIdmRequest **request_idm);
+// int _init_read_lvb(char *lock_id, char *host_id, char *lvb, int lvb_size,
+//                    char *drive, nvmeIdmRequest **request_idm);
+// int _init_read_mutex_group(char *drive, idmInfo **info_ptr, int *info_num,
+//                            nvmeIdmRequest **request_idm);
+// int _init_read_mutex_num(char *drive, unsigned int *num, nvmeIdmRequest **request_idm);
 int _init_unlock(char *lock_id, int mode, char *host_id, char *lvb, int lvb_size,
                  char *drive, nvmeIdmRequest **request_idm);
+
+// int _parse_host_state(nvmeIdmRequest *request_idm, int *host_state);
+// int _parse_lock_count(nvmeIdmRequest *request_idm, int *count);
+// int _parse_lock_mode(nvmeIdmRequest *request_idm, int *mode);
+// int _parse_lvb(nvmeIdmRequest *request_idm, char *lvb);
+// int _parse_mutex_group(nvmeIdmRequest *request_idm, idmInfo **info_ptr, int *info_num);
+// int _parse_mutex_num(nvmeIdmRequest *request_idm, int *num);
 
 
 //old
@@ -76,51 +109,3 @@ void _memory_free_idm_request(nvmeIdmRequest *request_idm);
 int _memory_init_idm_request(nvmeIdmRequest **request_idm, unsigned int data_num);
 int _validate_input_common(char *lock_id, char *host_id, char *drive);
 int _validate_input_write(char *lock_id, int mode, char *host_id, char *drive);
-
-
-
-
-
-//TODO: Temp reference for final naming layout
-// int nvme_async_idm_get_result(uint64_t handle, int *result);
-// int nvme_async_idm_get_result_host_state(uint64_t handle, int *host_state, int *result);
-// int nvme_async_idm_get_result_lock_count(uint64_t handle, int *count, int *result);
-// int nvme_async_idm_get_result_lock_mode(uint64_t handle, int *mode, int *result);
-// int nvme_async_idm_lock(char *lock_id, int mode, char *host_id, char *drive, uint64_t timeout);
-// int nvme_async_idm_lock_break(char *lock_id, int mode, char *host_id, char *drive, uint64_t timeout);
-// int nvme_async_idm_lock_convert(char *lock_id, int mode, char *host_id, char *drive, uint64_t timeout);
-// int nvme_async_idm_lock_destroy(char *lock_id, int mode, char *host_id, char *drive);
-// int nvme_async_idm_lock_refresh(char *lock_id, int mode, char *host_id, char *drive, uint64_t timeout);
-// int nvme_async_idm_lock_renew(char *lock_id, int mode, char *host_id, char *drive, uint64_t timeout);
-// int nvme_async_idm_read_host_state(char *lock_id, char *host_id, int *host_state, char *drive);
-// int nvme_async_idm_read_lock_count(char *lock_id, char *host_id, int *count, int *self, char *drive);
-// int nvme_async_idm_read_lock_mode(char *lock_id, int *mode, char *drive);
-// int nvme_async_idm_read_lvb(char *lock_id, char *host_id, char *lvb, int lvb_size, char *drive);
-// int nvme_async_idm_read_mutex_group(char *drive, idmInfo **info_ptr, int *info_num);
-// int nvme_async_idm_read_mutex_num(char *drive, unsigned int *num);
-// int nvme_async_idm_unlock(char *lock_id, int mode, char *host_id, char *lvb, int lvb_size, char *drive);
-// int nvme_sync_idm_lock(char *lock_id, int mode, char *host_id, char *drive, uint64_t timeout);
-// int nvme_sync_idm_lock_break(char *lock_id, int mode, char *host_id, char *drive, uint64_t timeout);
-// int nvme_sync_idm_lock_convert(char *lock_id, int mode, char *host_id, char *drive, uint64_t timeout);
-// int nvme_sync_idm_lock_destroy(char *lock_id, int mode, char *host_id, char *drive);
-// int nvme_sync_idm_lock_refresh(char *lock_id, int mode, char *host_id, char *drive, uint64_t timeout);
-// int nvme_sync_idm_lock_renew(char *lock_id, int mode, char *host_id, char *drive, uint64_t timeout);
-// int nvme_sync_idm_read_host_state(char *lock_id, char *host_id, int *host_state, char *drive);
-// int nvme_sync_idm_read_lock_count(char *lock_id, char *host_id, int *count, int *self, char *drive);
-// int nvme_sync_idm_read_lock_mode(char *lock_id, int *mode, char *drive);
-// int nvme_sync_idm_read_lvb(char *lock_id, char *host_id, char *lvb, int lvb_size, char *drive);
-// int nvme_sync_idm_read_mutex_group(char *drive, idmInfo **info_ptr, int *info_num);
-// int nvme_sync_idm_read_mutex_num(char *drive, unsigned int *num);
-// int nvme_sync_idm_unlock(char *lock_id, int mode, char *host_id, char *lvb, int lvb_size, char *drive);
-
-#endif /*__IDM_NVME_API_H__ */
-
-
-
-
-
-
-
-
-
-
