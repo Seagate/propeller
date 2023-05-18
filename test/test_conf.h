e321e1eee31bd85a73409ef642ba2d4ce6eae1b8
Scott Howe <scott.d.howe@seagate.com>
2023-05-18 09:20:04 -0500

    FOSS-541: Make device default names consisttent across code base.

--- test/test_conf.h
+++ test/test_conf.h
@@ -6,11 +6,12 @@
  */
 
 //TODO: Consolidate these settings, along with the duplicate python settings, in a .json file
-#define BLK_DEVICE1 "/dev/sd"
-#define BLK_DEVICE2 "/dev/sd"
-#define BLK_DEVICE3 "/dev/sd"
-#define BLK_DEVICE4 "/dev/sd"
-#define BLK_DEVICE5 "/dev/sd"
-#define BLK_DEVICE6 "/dev/sd"
-#define BLK_DEVICE7 "/dev/sd"
-#define BLK_DEVICE8 "/dev/sd"
+#define BLK_DEVICE1 "/dev/sdxxxxx"
+#define BLK_DEVICE2 "/dev/sdxxxxx"
+#define BLK_DEVICE3 "/dev/sdxxxxx"
+#define BLK_DEVICE4 "/dev/sdxxxxx"
+#define BLK_DEVICE5 "/dev/sdxxxxx"
+#define BLK_DEVICE6 "/dev/sdxxxxx"
+#define BLK_DEVICE7 "/dev/sdxxxxx"
+#define BLK_DEVICE8 "/dev/sdxxxxx"
+
